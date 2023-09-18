3d2dfeda844defd3c68b4eb161716e5c6b2324a9
Pawel Stenka <pawel.stenka@dynatrace.com>
2023-09-18 11:25:45 +0200

    Added Utility to termine whether ipv4 is external local

---
+++ libebpfdiscovery/test/IpUtilsTest.cpp
@@ -0,0 +1,58 @@
+#include <arpa/inet.h>
+#include <gtest/gtest.h>
+#include "ebpfdiscovery/IpUtils.h"
+#include <initializer_list>
+
+class IpUtilsTest : public IpUtils {
+protected:
+public:
+	using IpUtils::IpUtils;
+	void addIfceConfig(std::initializer_list<IpIfce> config) {
+		localNetsIpv4.insert(localNetsIpv4.end(), config.begin(), config.end());
+		bridgeEnd = moveBridges();
+	}
+};
+
+
+TEST(IpUtils, RawTest) {
+	IpUtils u;
+	u.readNetworks();
+	u.printAll();
+	const char* ip = "10.2.4.5";
+	std::cout << "E " << ip << " " << u.isAddresExternalLocal(inet_addr(ip)) << "\n";
+	ip = "172.17.4.5";
+	std::cout << "E " << ip << " " << u.isAddresExternalLocal(inet_addr(ip)) << "\n";
+	ip = "172.16.4.5";
+	std::cout << "E " << ip << " " << u.isAddresExternalLocal(inet_addr(ip)) << "\n";
+	ip = "192.168.4.5";
+	std::cout << "E " << ip << " " << u.isAddresExternalLocal(inet_addr(ip)) << "\n";
+	EXPECT_TRUE(true);
+}
+
+TEST(IpUtils, LocalBridgeIp) {
+	IpUtilsTest u;
+	u.addIfceConfig({{{inet_addr("10.2.4.5")}, {}, 0x0000ffff, 0, true}, {{inet_addr("10.7.4.5")}, {}, 0x0000ffff, 0, false}});
+	u.printAll();
+	EXPECT_FALSE(u.isAddresExternalLocal(inet_addr("10.2.6.5")));
+}
+
+TEST(IpUtils, NOTLocalBridgeIp) {
+	IpUtilsTest u;
+	u.addIfceConfig({{{inet_addr("10.2..6.5")}, {}, 0x0000ffff, 0, true}});
+	EXPECT_TRUE(u.isAddresExternalLocal(inet_addr("10.3.34.2")));
+}
+
+TEST(IpUtils, SimpleClassATest) {
+	IpUtilsTest u;
+	EXPECT_TRUE(u.isAddresExternalLocal(inet_addr("192.168.1.2")));
+}
+
+TEST(IpUtils, SimpleClassBTest) {
+	IpUtilsTest u;
+	EXPECT_TRUE(u.isAddresExternalLocal(inet_addr("172.20.21.2")));
+}
+
+TEST(IpUtils, SimpleClassCtest) {
+	IpUtilsTest u;
+	EXPECT_TRUE(u.isAddresExternalLocal(inet_addr("10.2.4.5")));
+}
