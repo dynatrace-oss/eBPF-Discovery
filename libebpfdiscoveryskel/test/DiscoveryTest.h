4d9c29f1c0c99869d4338157eb62f961c5e0dd06
Hubert Parzych <hubert.parzych@dynatrace.com>
2023-09-27 16:27:43 +0200

    test(libebpfdiscoveryskel): Implement unit tests for eBPF data utility functions (#11)

---
+++ libebpfdiscoveryskel/test/DiscoveryTest.h
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: GPL-2.0
+#pragma once
+
+#include "DiscoveryTestConstants.h"
+
+#include "discoveryTest.skel.h"
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+
+#include <gtest/gtest.h>
+
+namespace ebpfdiscovery::bpftest {
+
+class DiscoveryTest : public ::testing::Test {
+public:
+	DiscoveryTest() : testSkel{nullptr}, testBss{nullptr} {
+	}
+
+	bool isLoaded() {
+		return testSkel != nullptr && testBss != nullptr;
+	}
+
+	void setInPtr(const std::string& str) {
+		checkLoaded();
+		inPtrSrc.clear();
+		std::copy(str.begin(), str.end(), std::back_inserter(inPtrSrc));
+		testBss->inPtr = inPtrSrc.data();
+	}
+
+	void setInLen(size_t len) {
+		checkLoaded();
+		testBss->inLen = len;
+	}
+
+	int getOutRet() {
+		checkLoaded();
+		return testBss->outRet;
+	}
+
+protected:
+	void SetUp() override {
+		testSkel = discoveryTest_bpf__open_and_load();
+		if (testSkel == nullptr) {
+			GTEST_SKIP() << "Couldn't open and load BPF object for test execution.";
+		}
+
+		testBss = testSkel->bss;
+		testBss->runnerPid = getpid();
+	}
+
+	void TearDown() override {
+		if (testSkel != nullptr) {
+			discoveryTest_bpf__destroy(testSkel);
+		}
+	}
+
+	discoveryTest_bpf* testSkel;
+	discoveryTest_bpf::discoveryTest_bpf__bss* testBss;
+
+	std::vector<char> inPtrData;
+
+	void checkLoaded() {
+		if (!isLoaded()) {
+			throw std::runtime_error("DiscoveryTest is uninitialized");
+		}
+	}
+};
+
+void attachBpfProgram(bpf_program* prog) {
+	auto link = bpf_program__attach(prog);
+	if (link == nullptr) {
+		throw std::runtime_error("couldn't attach bpf program");
+	}
+}
+
+void triggerTracepoint() {
+	usleep(1);
+}
+
+} // namespace ebpfdiscovery::bpftest
---
+++ libebpfdiscoveryskel/test/DiscoveryTestConstants.h
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
+#pragma once
+
+#define DISCOVERY_TEST_MAX_INPUT_LEN 1024
