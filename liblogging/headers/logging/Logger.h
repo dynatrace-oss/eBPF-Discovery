568abb73637e4830a96088417c18a791773624ae
Hubert Parzych <hubert.parzych@dynatrace.com>
2023-10-10 13:06:26 +0200

    feat: Implement logging for ebpfdiscoverysrv (#19)

---
+++ liblogging/headers/logging/Logger.h
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
+#include <istream>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace logging {
+
+enum LogLevel {
+	Trace = static_cast<int>(spdlog::level::trace),
+	Debug = static_cast<int>(spdlog::level::debug),
+	Info = static_cast<int>(spdlog::level::info),
+	Warn = static_cast<int>(spdlog::level::warn),
+	Err = static_cast<int>(spdlog::level::err),
+	Critical = static_cast<int>(spdlog::level::critical),
+	Off = static_cast<int>(spdlog::level::off)
+};
+
+std::istream& operator>>(std::istream& in, LogLevel& level);
+
+class Logger {
+private:
+	static constexpr std::size_t max_size = 102400;
+	static constexpr std::size_t max_files = 3;
+
+public:
+	Logger();
+	~Logger() = default;
+	Logger(const Logger&) = delete;
+	Logger& operator=(const Logger&) = delete;
+
+	static Logger& getInstance();
+
+	void setLevel(enum LogLevel level);
+	void setup(std::string name, bool logToStdout = true, std::filesystem::path logDir = {});
+
+	template <typename... Args>
+	void log(enum LogLevel level, const char* fmt, const Args&... args) {
+		spdLogger.log(static_cast<spdlog::level::level_enum>(level), fmt, args...);
+	}
+
+	void vlogf(enum LogLevel level, const char* format, va_list args);
+
+	template <typename... Args>
+	void trace(const char* fmt, const Args&... args) {
+		spdLogger.trace(fmt, args...);
+	}
+
+	template <typename... Args>
+	void debug(const char* fmt, const Args&... args) {
+		spdLogger.debug(fmt, args...);
+	}
+
+	template <typename... Args>
+	void info(const char* fmt, const Args&... args) {
+		spdLogger.info(fmt, args...);
+	}
+
+	template <typename... Args>
+	void warn(const char* fmt, const Args&... args) {
+		spdLogger.warn(fmt, args...);
+	}
+
+	template <typename... Args>
+	void error(const char* fmt, const Args&... args) {
+		spdLogger.error(fmt, args...);
+	}
+
+	template <typename... Args>
+	void critical(const char* fmt, const Args&... args) {
+		spdLogger.critical(fmt, args...);
+	}
+
+	template <typename... Args>
+	void trace(const char* str) {
+		spdLogger.trace(str);
+	}
+
+	template <typename... Args>
+	void debug(const char* str) {
+		spdLogger.debug(str);
+	}
+
+	template <typename... Args>
+	void info(const char* str) {
+		spdLogger.info(str);
+	}
+
+	template <typename... Args>
+	void warn(const char* str) {
+		spdLogger.warn(str);
+	}
+
+	template <typename... Args>
+	void error(const char* str) {
+		spdLogger.error(str);
+	}
+
+	template <typename... Args>
+	void critical(const char* str) {
+		spdLogger.critical(str);
+	}
+
+private:
+	void loggerSetDefaults();
+	void setLogger(spdlog::logger logger);
+	void logLine(enum LogLevel level, const char* str, size_t len);
+
+	spdlog::logger spdLogger;
+};
+
+} // namespace logging
+
+#define LOG_TRACE(...) logging::Logger::getInstance().trace(__VA_ARGS__)
+#define LOG_DEBUG(...) logging::Logger::getInstance().debug(__VA_ARGS__)
+#define LOG_INFO(...) logging::Logger::getInstance().info(__VA_ARGS__)
+#define LOG_WARN(...) logging::Logger::getInstance().warn(__VA_ARGS__)
+#define LOG_ERROR(...) logging::Logger::getInstance().error(__VA_ARGS__)
+#define LOG_CRITICAL(...) logging::Logger::getInstance().critical(__VA_ARGS__)
---
+++ liblogging/src/Logger.cpp
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "logging/Logger.h"
+
+#include <spdlog/common.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include <algorithm>
+#include <cstdarg>
+#include <filesystem>
+#include <memory>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+namespace logging {
+
+std::istream& operator>>(std::istream& in, LogLevel& level) {
+	std::string token;
+	in >> token;
+	std::transform(token.begin(), token.end(), token.begin(), [](unsigned char c) { return std::tolower(c); });
+	if (token == "trace") {
+		level = LogLevel::Trace;
+	} else if (token == "debug") {
+		level = LogLevel::Debug;
+	} else if (token == "info") {
+		level = LogLevel::Info;
+	} else if (token == "warning" || token == "warn") {
+		level = LogLevel::Warn;
+	} else if (token == "error" || token == "err") {
+		level = LogLevel::Err;
+	} else if (token == "critical" || token == "crit") {
+		level = LogLevel::Critical;
+	} else if (token == "off") {
+		level = LogLevel::Off;
+	} else {
+		in.setstate(std::ios::failbit);
+	}
+	return in;
+}
+
+Logger::Logger() : spdLogger("") {
+	loggerSetDefaults();
+}
+
+Logger& Logger::getInstance() {
+	static Logger instance;
+	return instance;
+}
+
+void Logger::setLevel(enum LogLevel level) {
+	spdLogger.set_level(static_cast<spdlog::level::level_enum>(level));
+}
+
+void Logger::setup(std::string name, bool logToStdout, std::filesystem::path logDir) {
+	namespace fs = std::filesystem;
+
+	std::vector<spdlog::sink_ptr> sinks;
+	if (logToStdout) {
+		sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	}
+
+	if (!logDir.empty()) {
+		if (!fs::exists(logDir)) {
+			throw std::runtime_error("Log directory doesn't exist");
+		}
+
+		if (!fs::is_directory(logDir)) {
+			throw std::runtime_error("Log directory is not a directory");
+		}
+
+		if (access(logDir.c_str(), R_OK | W_OK) != 0) {
+			throw std::runtime_error("Couldn't access log directory for reading and writing");
+		}
+
+		fs::path logFile{logDir / (name + ".log")};
+		sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(logFile, max_size, max_files));
+	}
+
+	setLogger(spdlog::logger(name, sinks.begin(), sinks.end()));
+}
+
+void Logger::vlogf(enum LogLevel level, const char* format, va_list args) {
+	if (!spdLogger.should_log(static_cast<spdlog::level::level_enum>(level))) {
+		return;
+	}
+
+	const int staticBufSize{512};
+	static char staticBuf[staticBufSize]{};
+
+	static va_list argsCopy;
+	va_copy(argsCopy, args);
+
+	int resultSize{vsnprintf(staticBuf, staticBufSize, format, args)};
+	if (resultSize <= staticBufSize) {
+		logLine(level, staticBuf, resultSize);
+		return;
+	}
+
+	char buf[resultSize]{};
+	vsnprintf(buf, resultSize, format, argsCopy);
+
+	logLine(level, buf, resultSize);
+}
+
+void Logger::setLogger(spdlog::logger logger) {
+	spdLogger = std::move(logger);
+	loggerSetDefaults();
+}
+
+void Logger::loggerSetDefaults() {
+	spdLogger.set_level(spdlog::level::off);
+	spdLogger.set_pattern("%Y-%m-%d %H:%M:%S.%e [%t] %^%l%$ [%n] %v");
+}
+
+void Logger::logLine(enum LogLevel level, const char* buf, size_t len) {
+	if (buf == nullptr || len == 0) {
+		return;
+	}
+
+	std::string str;
+	if (len > 1 && buf[len - 1] == '\n') {
+		str.assign(buf, len - 1);
+	} else {
+		str.assign(buf, len);
+	}
+
+	log(level, "{}", str);
+}
+
+} // namespace logging
