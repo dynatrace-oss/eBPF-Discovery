887e661dace3e7fed5bd5b0f02496302bc4c3b04
Przemyslaw Susko <przemyslaw.susko@dynatrace.com>
2023-10-31 11:24:46 +0100

    run tests from root + PR comments

--- testing/conftest.py
+++ testing/conftest.py
@@ -19,7 +19,7 @@ def discovery_path(pytestconfig):
 
 
 @pytest.fixture(scope="session")
-def ebpf_discovery(discovery_path):
+def run_ebpf_discovery(discovery_path):
     args = (discovery_path, "--interval", "2")
     discovery = subprocess.Popen(args, stdout=subprocess.PIPE)
     yield discovery
@@ -36,11 +36,11 @@ def ebpf_discovery(discovery_path):
 
 
 @pytest.fixture(scope="session")
-def http_service(docker_ip, docker_services, ebpf_discovery):
+def http_service(docker_ip, docker_services, run_ebpf_discovery):
     port = docker_services.port_for("httpbin", 80)
     url = "http://{}:{}/".format(docker_ip, port)
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
     )
-    assert discovered_service_has_clients(ebpf_discovery, url, 1, 0)
+    assert discovered_service_has_clients(run_ebpf_discovery, url, 1, 0)
     return url
--- testing/test_discovery.py
+++ testing/test_discovery.py
@@ -1,13 +1,13 @@
 from utils import discovered_service_has_clients, send_http_requests
 
 
-def test_service_discovery(ebpf_discovery, http_service):
+def test_service_discovery(run_ebpf_discovery, http_service):
     url = http_service + "some/url"
     requests_num = 5
     send_http_requests(url, requests_num)
-    assert discovered_service_has_clients(ebpf_discovery, url, requests_num, 0)
+    assert discovered_service_has_clients(run_ebpf_discovery, url, requests_num, 0)
 
     url = http_service + "other/url"
     requests_num = 10
     send_http_requests(url, requests_num)
-    assert discovered_service_has_clients(ebpf_discovery, url, requests_num, 0)
+    assert discovered_service_has_clients(run_ebpf_discovery, url, requests_num, 0)
