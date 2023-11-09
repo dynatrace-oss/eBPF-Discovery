dcff5e137fbc67ffbdaeb91d03a531e1333617e3
Przemyslaw Susko <przemyslaw.susko@dynatrace.com>
2023-11-09 11:34:31 +0100

    Rename test dir

---
+++ test/utils.py
@@ -0,0 +1,68 @@
+import json
+import logging
+import subprocess
+import time
+
+import requests
+
+
+def send_http_requests(url: str, requests_num: int, x_forwarded_for: str | None = None):
+    headers = {}
+    if x_forwarded_for:
+        headers.update({"X-Forwarded-For": x_forwarded_for})
+    for i in range(requests_num):
+        requests.get(url, headers=headers)
+
+
+def is_responsive(url: str) -> bool:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+    except (requests.ConnectionError, requests.ConnectTimeout, requests.Timeout):
+        return False
+
+
+def wait_until(predicate, timeout=2, period=0.25):
+    while timeout > 0:
+        if predicate():
+            return True
+        time.sleep(period)
+        timeout -= period
+    return False
+
+
+def get_discovered_service_json(discovery: subprocess.Popen, url: str) -> dict | None:
+    def url_matches_endpoint(url, endpoint):
+        return url[len("http://"):] == endpoint
+
+    output = discovery.stdout.readline()
+    if not output:
+        return None
+    try:
+        services_json = json.loads(output)
+        for service in services_json["service"]:
+            if url_matches_endpoint(url, service["endpoint"]):
+                return service
+    except (json.decoder.JSONDecodeError, KeyError):
+        return None
+    return None
+
+
+def discovered_service_has_clients(discovery: subprocess.Popen, url: str, local_clients_number: int,
+                                   external_clients_number: int) -> bool:
+    service = get_discovered_service_json(discovery, url)
+    if not service:
+        logging.warning("No discovered service for endpoint {}".format(url))
+        return False
+    if local_clients_number:
+        if service.get("internalClientsNumber", 0) != local_clients_number:
+            logging.warning("Internal clients number mismatch ({}!={}) for endpoint {}"
+                            .format(service.get("internalClientsNumber", "0"), local_clients_number, url))
+            return False
+    if external_clients_number:
+        if service.get("externalClientsNumber", 0) != external_clients_number:
+            logging.warning("External clients number mismatch ({}!={}) for endpoint {}"
+                            .format(service.get("externalClientsNumber", "0"), external_clients_number, url))
+            return False
+    return True
