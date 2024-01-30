ee93601bde7df9c1ae3bc47b1d0e494338c0b23b
Jakub Rak <jakub.rak@dynatrace.com>
2024-01-30 16:02:50 +0100

    Prepare for publishing (#73)
    * Rename thirdparty dir to third_party
    
    * Move bcc code to third_party
    
    * Remove libbpf duplicate
    
    * Move cmake third party files
    
    * Add missing license files
    
    * Add license headers

--- ebpfdiscoverysrv/src/main.cpp
+++ ebpfdiscoverysrv/src/main.cpp
@@ -1,4 +1,19 @@
-// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2023 Dynatrace LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "ebpfdiscovery/Discovery.h"
 #include "ebpfdiscovery/DiscoveryBpf.h"
 #include "ebpfdiscovery/DiscoveryBpfLogging.h"
@@ -41,7 +56,7 @@ static po::options_description getProgramOptions() {
 }
 
 static void initLogging(logging::LogLevel logLevel, bool enableStdout, const std::filesystem::path& logDir) {
-	Logger::getInstance().init( enableStdout, logDir);
+	Logger::getInstance().init(enableStdout, logDir);
 	Logger::getInstance().setLevel(logLevel);
 	LOG_TRACE("Logging has been set up. (enableStdout: {}, logDir: `{}`)", enableStdout, logDir.string());
 }
