ca494c4d73629b06f043f7d3ce702e3a5c58bcae
Jakub Rak <jakub.rak@dynatrace.com>
2024-01-30 16:02:50 +0100

    Prepare for publishing (#73)
    * Rename thirdparty dir to third_party
    
    * Move bcc code to third_party
    
    * Remove libbpf duplicate
    
    * Move cmake third party files
    
    * Add missing license files
    
    * Add license headers

--- third_party/bcc/libbpf-tools/trace_helpers.c
+++ third_party/bcc/libbpf-tools/trace_helpers.c
@@ -6,21 +6,21 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
+#include "trace_helpers.h"
+#include "bpf/bpf.h"
+#include "bpf/btf.h"
+#include "bpf/libbpf.h"
+#include "uprobe_helpers.h"
 #include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <sys/resource.h>
 #include <time.h>
-#include <bpf/bpf.h>
-#include <bpf/btf.h>
-#include <bpf/libbpf.h>
-#include <limits.h>
-#include "trace_helpers.h"
-#include "uprobe_helpers.h"
+#include <unistd.h>
 
 #define min(x, y) ({				\
 	typeof(x) _min1 = (x);			\
