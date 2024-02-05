4d6c28deb2155fe55b42cc7dd217eafa506ea612
Filip Szweda <filip.szweda@dynatrace.com>
2024-02-05 09:19:50 +0100

    Fix

--- libebpfdiscoveryskel/src/SyscallProbes.h
+++ libebpfdiscoveryskel/src/SyscallProbes.h
@@ -30,6 +30,7 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <sys/socket.h>
 
 /*
  * Maps for storing syscall arguments to pass them from kprobes to kretprobes.
