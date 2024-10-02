41774630c8c0295564a5039b245436291871d3a5
Filip Szweda <filip.szweda@dynatrace.com>
2024-10-02 14:12:07 +0200

    Update copyrights to 2024

--- libebpfdiscoveryskel/src/Log.h
+++ libebpfdiscoveryskel/src/Log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Dynatrace LLC
+ * Copyright 2024 Dynatrace LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
