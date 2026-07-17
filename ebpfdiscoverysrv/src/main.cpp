2aef986c2fbcca8024591a611850ca143a75c8f7
Arkadiusz Sienkiewicz <arkadiusz.sienkiewicz@dynatrace.com>
2026-07-17 09:14:23 +0200

    Revert premature changes

--- ebpfdiscoverysrv/src/main.cpp
+++ ebpfdiscoverysrv/src/main.cpp
@@ -69,7 +69,7 @@ po::options_description getProgramOptions() {
 	  (logNoStdoutName.data(), po::bool_switch()->default_value(false), "Disable logging to stdout")
 	  (versionName.data(), "Display program version")
 	  //TODO change default value to false after DT OA starts using this switch for service detection
-	  (enableServiceDetectionName.data(), po::bool_switch()->default_value(false), "Enables service detection")
+	  (enableServiceDetectionName.data(), po::bool_switch()->default_value(true), "Enables service detection")
 	  (intervalName.data(), po::value<int>()->default_value(60), "Services reporting time interval (in seconds)")
 	  (enableNetworkCountersName.data(), po::bool_switch()->default_value(false), "Enable network counters")
 	  (enableSlpName.data(), po::bool_switch()->default_value(false), "Enables the short-lived-process detection")
@@ -139,7 +139,8 @@ int main(int argc, char** argv) {
 	}
 
 	const bool enableSlp{vm[enableSlpName.data()].as<bool>()};
-	const bool enableServiceDetection{vm[enableServiceDetectionName.data()].as<bool>()};
+	//TODO !enableSlp is a temporary workaround to allow gathering only slp, remove when changing enableServiceDetection default value to false
+	const bool enableServiceDetection = !enableSlp && vm[enableServiceDetectionName.data()].as<bool>();
 
 	if (!enableSlp && !enableServiceDetection) {
 		return EXIT_SUCCESS;
